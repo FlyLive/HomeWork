a889db05f1f9196421a03115ebfe9a0195208080
FlyLive <1585213801@qq.com>
2017-04-14 16:01:09 +0800

    Default

--- src/Chapter1/Exercise1_1.java
+++ src/Chapter1/Exercise1_1.java
@@ -2,6 +2,6 @@ package Chapter1;
 
 public class Exercise1_1 {
 	public static void main(String[] args){
-		System.out.println("Welcome to Java¡¢Welcome to Computer Science¡¢Programing is fun");
+		System.out.println("Welcome to Java,Welcome to Computer Science,Programing is fun");
 	}
 }
